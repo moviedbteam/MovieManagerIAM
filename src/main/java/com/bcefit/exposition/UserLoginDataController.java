f86a6b67c02653fb77e6b7af081d7bdd9617def7
DESKTOP-F1TL2CH\jchay <joel.chaysinh@epita.fr>
2023-03-09 16:29:38 +0100

    Désactivation du chargement data.sql/API create modifiée

--- src/main/java/com/bcefit/exposition/UserLoginDataController.java
+++ src/main/java/com/bcefit/exposition/UserLoginDataController.java
@@ -2,6 +2,7 @@ package com.bcefit.exposition;
 
 import com.bcefit.application.IUserLoginDataService;
 import com.bcefit.dto.UserLoginDataDto;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,7 +27,11 @@ public class UserLoginDataController {
         //Appel du service pour insérer en BDD
         userLoginDataService.save(userLoginDataDto);
 
-        return new ResponseEntity<>("USER CREATED", HttpStatus.CREATED);
+        JsonObject jsonResponse = new JsonObject();
+        jsonResponse.addProperty("email", userLoginDataDto.getEmail());
+        jsonResponse.addProperty("pseudo", userLoginDataDto.getLoginName());
+
+        return new ResponseEntity<>(jsonResponse.toString(), HttpStatus.CREATED);
     }
 
 }
