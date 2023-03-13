f781f6a319355da627d58dcfb6ec771c1f3a9f32
DESKTOP-F1TL2CH\jchay <joel.chaysinh@epita.fr>
2023-03-13 17:51:29 +0100

    MAJ durée du Token de 15min à 24h

--- src/main/java/com/bcefit/security/SecurityConstants.java
+++ src/main/java/com/bcefit/security/SecurityConstants.java
@@ -4,6 +4,6 @@ package com.bcefit.security;
 public class SecurityConstants {
 
     public static final String SECRET = "monSecret123456789";
-    public static final long EXPIRATION_TIME = 900_000; // 15 mins
+    public static final long EXPIRATION_TIME = 86_400_000; // 24h (pour info 15 mins = 900_00)
 
 }
